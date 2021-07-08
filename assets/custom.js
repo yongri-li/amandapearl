bbbe983532e8844b995d7ab85233e4dbd857a926
yongri-li <yongri.liii+1@gmail.com>
2021-07-09 00:31:20 +0200

    update product images zoom slider

---
+++ assets/custom.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+  if($(window).width() > 767) {
+    productGalleryPopup();
+  }
+
+})
+
+
+function productGalleryPopup() {
+
+  var $popup = $(".product-gallery-popup");
+
+  $(document).on('click', '[data-gallery-trigger]', function() {
+    $popup.fadeIn(300);
+    var idx = $(this).attr('data-idx');
+    setTimeout(function() {
+      galleryImgSlide(idx);
+    }, 250);
+  })
+
+  $(document).on('click', '[data-gallery-thumbs] [data-gallery-item]', function() {
+    var idx = $(this).attr('data-idx')
+    galleryImgSlide(idx);
+  })
+
+  $(document).on('click', '.product-gallery-popup [data-popup-close]', function() {
+    $popup.fadeOut();
+  })
+}
+
+function galleryPopupOpen() {
+
+}
+
+
+function galleryImgSlide(idx) {
+  var $content = $('[data-gallery-imgs] .gallery-images-wrapper');
+  var $target = $('[data-gallery-imgs] [data-gallery-item][data-idx=' + idx + ']');
+  var top = 0;
+  for(var i = 0; i < idx; i ++) {
+    var $item = $('[data-gallery-imgs] [data-gallery-item][data-idx=' + idx + ']');
+    top += $item.height() + 15;
+  }
+
+  console.log(idx + "::::" + top);
+  $content.animate({
+    scrollTop: top
+  }, 800);
+
+}
